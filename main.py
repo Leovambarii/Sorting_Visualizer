87b340983264c446be133b739b878133c8873d94
Leovambarii <ksprp121@gmail.com>
2023-03-25 00:33:48 +0100

    Optimalization and added comments

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import tkinter as tk
 import customtkinter as ctk
 import random
 import time
-from copy import deepcopy
 
 # colors
 DARK_BLUE_GRAY = '#52527a'
@@ -17,14 +16,16 @@ BARS_AMOUNT = 100
 
 # speed modes
 SLOW = 0.05
-NORMAL = 0.005
+NORMAL = 0.01
 FAST = 0
 
+# convert hex color to rgv
 def hex_to_rgb(value):
     value = value.lstrip('#')
     lv = len(value)
     return tuple(int(value[i:i+lv//3], 16) for i in range(0, lv, lv//3))
 
+# change rgb brightness
 def change_rgb_brightness(color, height):
     r, g, b = color
     r = int(r * height)
@@ -32,18 +33,22 @@ def change_rgb_brightness(color, height):
     b = int(b * height)
     return (r, g, b)
 
+# convert rgb color to hex
 def rgb_to_hex(rgb):
     r, g, b = rgb
     return '#{:02x}{:02x}{:02x}'.format(r, g, b)
 
+# setting standard interface appearance
 ctk.set_appearance_mode("Dark")
 ctk.set_default_color_theme("green")
 
+# a class for storing value and color for each bar
 class Data:
     def __init__(self, value, color):
         self.value = value
         self.color = color
 
+# Main class
 class App(ctk.CTk):
     def __init__(self):
         super().__init__()
@@ -107,6 +112,7 @@ class App(ctk.CTk):
         self.fast_radiobtn = ctk.CTkRadioButton(master=self.speed_radiobtn_frame, text="Fast", variable=self.speed_mode, value=FAST)
         self.fast_radiobtn.grid(row=3, column=0, pady=5, padx=10, sticky='n')
 
+        # interface appearance modes oprion menu
         self.appearance_mode_label = ctk.CTkLabel(self.menu_frame, text="APPEARENCE MODE", anchor="w")
         self.appearance_mode_label.grid(row=5, column=0, padx=20, pady=(10, 0))
         self.appearance_mode_optionemenu = ctk.CTkOptionMenu(self.menu_frame, values=["Dark", "Light"], command=self.change_appearance_mode)
@@ -117,21 +123,21 @@ class App(ctk.CTk):
         self.normal_radiobtn.select() # select default speed - Normal
         self.sort_button.configure(state="disabled")
 
+    # change appearance of the interface
     def change_appearance_mode(self, new_appearance_mode: str):
         ctk.set_appearance_mode(new_appearance_mode)
 
     # generate random data
     def generate(self):
         self.data = [Data(random.randint(1, MAX_VALUE), BARS_COLOR) for _ in range(100)]
-        self.visualise()
+        self.visualise(generate_flag=True)
         self.sort_button.configure(state="normal")
 
     # draw data as vertical bars
-    def visualise(self):
+    def visualise(self, generate_flag=False):
         self.canvas.delete("all")
         x_width = self.canvas_width / (len(self.data)+1)
         offset = 5
-
         data_norm = [x.value / MAX_VALUE for x in self.data]
 
         for i, height in enumerate(data_norm):
@@ -141,21 +147,26 @@ class App(ctk.CTk):
             y1 = self.canvas_height
 
             # convert hex color to rgb to change brightness depending on height
-            color_rgb = hex_to_rgb(self.data[i].color)
-            new_rgb_color = change_rgb_brightness(color_rgb, height)
-            new_color = rgb_to_hex(new_rgb_color)
+            if generate_flag:
+                color_rgb = hex_to_rgb(self.data[i].color)
+                new_rgb_color = change_rgb_brightness(color_rgb, height)
+                new_color = rgb_to_hex(new_rgb_color)
+                self.data[i].color = new_color
 
             # drawing vertical bar
-            self.canvas.create_rectangle(x0, y0, x1, y1, fill=new_color)
+                self.canvas.create_rectangle(x0, y0, x1, y1, fill=new_color)
+            else:
+                self.canvas.create_rectangle(x0, y0, x1, y1, fill=self.data[i].color)
 
         self.update_idletasks()
 
-    # sort the data
+    # running selected sort function
     def sorting(self):
+        # blocking unwanted buttons
         self.generate_button.configure(state="disabled")
         self.sort_button.configure(state="disabled")
-        algorithm_type = self.algorithm_type.get()
 
+        algorithm_type = self.algorithm_type.get()
         if algorithm_type == 'Bubble':
             self.bubble()
         elif algorithm_type == 'Merge':
@@ -164,11 +175,11 @@ class App(ctk.CTk):
         self.generate_button.configure(state="normal")
         self.sort_button.configure(state="disabled")
 
+    # ---------- SORTING ALGORITHMS ----------
     # bubble sort
     def bubble(self, i=0):
         time_sleep = self.speed_mode.get()
         size = len(self.data)
-
         for i in range(size-1):
             for j in range(size-i-1):
                 if self.data[j].value > self.data[j+1].value:
@@ -188,15 +199,12 @@ class App(ctk.CTk):
             else:
                 temp.append(self.data[j])
                 j += 1
-
         while i <= mid:
             temp.append(self.data[i])
             i += 1
-
         while j <= right:
             temp.append(self.data[j])
             j += 1
-
         i, k = 0, right-left+1
         while i < k:
             self.data[left+i] = temp[i]
@@ -208,13 +216,12 @@ class App(ctk.CTk):
             mid = (left + right) // 2
             self.merge(left, mid)
             self.merge(mid+1, right)
-
             self.mrg(left, mid, right)
             self.visualise()
             time.sleep(time_sleep)
-
         self.visualise()
 
+# Main loop
 if __name__ == "__main__":
     app = App()
     app.mainloop()
